a17fd4fef14878a1541f81c551dfc2246d299e48
Sebastian <sebastianm187@live.com>
2017-03-20 22:22:44 +0100

    refactor, dotnet-list

--- dotnet.sh
+++ dotnet.sh
@@ -1,75 +1,215 @@
 _dotnet()
 {
-    local cur prev prev2 cmd opts
-    COMPREPLY=()
-    cur="${COMP_WORDS[COMP_CWORD]}"
-    prev="${COMP_WORDS[COMP_CWORD-1]}"
-    prev2="${COMP_WORDS[COMP_CWORD-2]}"
-    cmd=$"${COMP_WORDS[1]}"
+	local cur prev prev2 cmd opts
+	COMPREPLY=()
+	cur="${COMP_WORDS[COMP_CWORD]}"
+	prev="${COMP_WORDS[COMP_CWORD-1]}"
+	prev2="${COMP_WORDS[COMP_CWORD-2]}"
+	cmd=$"${COMP_WORDS[1]}"
 
-    case "${cmd}" in
-      new)
-        if [[ ${prev} == new ]] ; then
-          opts="console classlib mstest xunit web mvc webapi sln"
-          COMPREPLY=( $(compgen -W "${opts}" ${cur}) )
-          return 0
-        fi
+	case "${cmd}" in
+		new)
+			_dotnet_new
+		;;
 
-        if [[ ${cur} == -* ]] ; then
-          opts="--list --language --name --output --help"
-          COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
-          return 0
-        fi
+		restore)
+			_dotnet_restore
+		;;
 
-        onlycs="web webapi";
-        if [[ ${prev} == --language && $onlycs == *${prev2}* ]] ; then
-          opts="C#"
-          COMPREPLY=( $(compgen -W "${opts}" ${cur}) )
-          return 0
-        fi
+		build)
+			_dotnet_build
+		;;
 
-        if [[ ${prev} == --language ]] ; then
-          opts="C# F#"
-          COMPREPLY=( $(compgen -W "${opts}" ${cur}) )
-          return 0
-        fi
-        ;;
+		publish)
+			_dotnet_publish
+		;;
 
-      add)
-        if [[ ${prev} == add ]] ; then
-          opts="package reference"
-          COMPREPLY=( $(compgen -W "${opts}" ${cur}) )
-          return 0
-        fi
+		run)
+			_dotnet_run
+		;;
 
-        if [[ ${prev} == package ]] ; then
-          opts="$(curl -s https://api-v2v3search-0.nuget.org/autocomplete?q=${cur} | grep -Po '\[.*?\]' | grep -Po '(?<=").*?(?=")')"
-          COMPREPLY=( $(compgen -W "${opts}" ${cur}) )
-          return 0
-        fi
+		test)
+			_dotnet_test
+		;;	
 
-        if [[ ${cur} == -* ]] ; then
-          opts="--version"
-          COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
-          return 0
-        fi
+		pack)
+			_dotnet_pack
+		;;
 
-        if [[ ${prev} == --version ]] ; then
-          opts="$(curl -s https://api-v2v3search-0.nuget.org/autocomplete?id=${prev2} | grep -Po '\[.*?\]' | grep -Po '(?<=").*?(?=")' | grep -Po '^[^,]+$' | sort)"
-          COMPREPLY=( $(compgen -W "${opts}" ${cur}) )
-          return 0
-        fi
-        ;;
-      *)
-      ;;
-    esac
+		migrate)
+			_dotnet_migrate
+		;;
+		
+		clean)
+			_dotnet_clean
+		;;
 
-    if [[ ${prev} == dotnet ]] ; then
-      opts="new restore build publish run test pack migrate clean sln add remove list"
-      COMPREPLY=( $(compgen -W "${opts}" ${cur}) )
-      return 0
-    fi
-   
+		sln)
+			_dotnet_sln
+		;;
+
+		add)
+			_dotnet_add
+		;;
+
+		remove)
+			_dotnet_remove
+		;;
+
+		list)
+			_dotnet_list
+		;;
+
+		*)
+		;;
+	esac
+
+	if [[ ${prev} == dotnet ]] ; then
+		opts="new restore build publish run test pack migrate clean sln add remove list nuget msbuild vstest"
+		COMPREPLY=( $(compgen -W "${opts}" ${cur}) )
+		return 0
+	fi
+
+}
+
+_dotnet_new()
+{
+	case "${prev}" in 
+		new)
+			opts="console classlib mstest xunit web mvc webapi sln"
+			COMPREPLY=( $(compgen -W "${opts}" ${cur}) )
+			return 0
+		;;
+
+		--language)
+			opts="$(_get_languages ${prev2})"
+			COMPREPLY=( $(compgen -W "${opts}" ${cur}) )
+			return 0
+		;;
+
+		*)
+			opts="--list --language --name --output --help"
+			COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+			return 0
+		;;
+	esac
+}
+
+_get_languages()
+{
+	local template
+	template=$1
+
+	if [[ "web webapi" == *${template}* ]] ; then
+		echo "C#"
+		return 0
+	fi
+
+	echo "C# F#"
+	return 0
+}
+
+_dotnet_restore()
+{
+	#TODO
+	return 0;
+}
+
+_dotnet_build()
+{
+	#TODO
+	return 0;
+}
+
+_dotnet_publish()
+{
+	#TODO
+	return 0;
+}
+
+_dotnet_run()
+{
+	#TODO
+	return 0;
+}
+
+_dotnet_test()
+{
+	#TODO
+	return 0;
+}
+
+_dotnet_pack()
+{
+	#TODO
+	return 0;
+}
+
+_dotnet_migrate()
+{
+	#TODO
+	return 0;
+}
+
+_dotnet_clean()
+{
+	#TODO
+	return 0;
+}
+
+_dotnet_sln()
+{
+	#TODO
+	return 0;
+}
+
+
+_dotnet_add()
+{
+	case "${prev}" in
+		add)
+			opts="package reference"
+			COMPREPLY=( $(compgen -W "${opts}" ${cur}) )
+			return 0
+		;;
+
+		package)
+			opts="$(curl -s https://api-v2v3search-0.nuget.org/autocomplete?q=${cur} | grep -Po '\[.*?\]' | grep -Po '(?<=").*?(?=")' | grep -Po '^[^,]+$' | sort)"
+			COMPREPLY=( $(compgen -W "${opts}" ${cur}) )
+			return 0
+		;;
+
+		--version)
+			opts="$(curl -s https://api-v2v3search-0.nuget.org/autocomplete?id=${prev2} | grep -Po '\[.*?\]' | grep -Po '(?<=").*?(?=")' | grep -Po '^[^,]+$' | sort)"
+			COMPREPLY=( $(compgen -W "${opts}" ${cur}) )
+			return 0
+		;;
+
+		*)
+			opts="--version"
+			COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+			return 0
+		;;
+	esac
+}
+
+_dotnet_remove()
+{
+	#TODO
+	return 0;
+}
+
+_dotnet_list()
+{
+	if [[ ${prev} == list ]] ; then
+		opts="$(find . -name \*.csproj -print)"
+		COMPREPLY=( $(compgen -W "${opts}" ${cur}) )
+		return 0
+	else
+		opts="reference"
+		COMPREPLY=( $(compgen -W "${opts}" ${cur}) )
+		return 0
+	fi
 }
 
 complete -F _dotnet dotnet
